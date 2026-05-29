ac516af1671c6a254a6c6bb71f20d9e38dc0e2f5
gopika <gopikag.info@gmail.com>
2026-05-29 19:31:26 +0530

    Restore original working database configurations

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,7 @@
 var mongoose = require("mongoose");
 
-// Updated to use the clean 'bloodadmin' user credentials
 mongoose
-    .connect("mongodb+srv://bloodadmin:Password123@cluster0.wvbgeuy.mongodb.net/blood?retryWrites=true&w=majority")
+    .connect("mongodb+srv://text:test@cluster0.wvbgeuy.mongodb.net/blood?retryWrites=true&w=majority")
     .then(() => {
         console.log("db connected");
     })
