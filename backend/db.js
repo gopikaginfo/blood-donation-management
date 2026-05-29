1317de528a8b22e770bbcb73819c35db3b636b62
gopika <gopikag.info@gmail.com>
2026-05-29 13:29:28 +0530

    Fix database connection user password authentication

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,8 @@
 var mongoose = require("mongoose");
 
+// Configured with your verified database user 'text' and new password 'gopika123'
 mongoose
-    .connect("mongodb+srv://text:test@cluster0.wvbgeuy.mongodb.net/blood?retryWrites=true&w=majority")
+    .connect("mongodb+srv://text:gopika123@cluster0.wvbgeuy.mongodb.net/blood?retryWrites=true&w=majority")
     .then(() => {
         console.log("db connected");
     })
