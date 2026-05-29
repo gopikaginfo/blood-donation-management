ad3481e27718fb20d9c37e3c7d55ce04f3e08de0
gopika <gopikag.info@gmail.com>
2026-05-29 19:23:58 +0530

    Deploy fresh database user authentication

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,8 @@
 var mongoose = require("mongoose");
 
-// Configured with your verified database user 'text' and new password 'gopika123'
+// Updated to use the clean 'bloodadmin' user credentials
 mongoose
-    .connect("mongodb+srv://text:gopika123@cluster0.wvbgeuy.mongodb.net/blood?retryWrites=true&w=majority")
+    .connect("mongodb+srv://bloodadmin:Password123@cluster0.wvbgeuy.mongodb.net/blood?retryWrites=true&w=majority")
     .then(() => {
         console.log("db connected");
     })
