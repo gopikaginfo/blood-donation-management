d8e91946bc5c015f807d5a0a691a818609eb6fad
gopikaginfo <gopikag.info@gmail.com>
2026-05-30 20:38:39 +0530

    Update MongoDB connection string and logging

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,10 @@
 var mongoose = require("mongoose");
 
 mongoose
-  .connect(
-    "mongodb+srv://bloodadmin:blood123@cluster0.wvbgeuy.mongodb.net/blood?retryWrites=true&w=majority&appName=Cluster0"
-  )
-  .then(() => {
-    console.log("DB Connected");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+    .connect("mongodb+srv://text:test@cluster0.wvbgeuy.mongodb.net/blood?retryWrites=true&w=majority")
+    .then(() => {
+        console.log("db connected ✅");
+    })
+    .catch((err) => {
+        console.log("Database connection error:", err);
+    });
